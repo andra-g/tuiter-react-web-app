c760756ba52351bf6cbf052447a2e5feb9be1eb2
Andra <andra.ioana2002@gmail.com>
2023-03-05 15:22:16 -0500

    netlify fix

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,7 +1,7 @@
 
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import WhoToFollowList from "../WhoToFollowList/index.js";
-import PostSummaryList from "../PostSummaryList";
+import PostSummaryList from "../PostSummaryList/index.js";
 import ExploreComponent from "./ExploreComponent.js";
 
 function exploreComponent() {
